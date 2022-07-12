cd0fa4d9e283240068db2fa01ecbf70e576f6641
Sebastian <101427281+SebaSuffer@users.noreply.github.com>
2022-07-12 00:18:14 -0400

    Update bicola.h

--- bicola.h
+++ bicola.h
@@ -1,1 +1,24 @@
 
+#ifndef bicola_h
+#define bicola_h
+
+typedef struct Elemento Elemento;
+typedef struct Bicola Bicola;
+
+Bicola * crearBicola();
+
+int esVacía(Bicola* bicola);
+
+Elemento* createElem(void * dato);
+
+void insertarIzq(Bicola* bicola, void * value);
+
+void insertarDer(Bicola* bicola, void * value);
+
+void* eliminarIzq(Bicola* bicola);
+
+void* eliminarDer(Bicola* bicola);
+
+void enLarge(Bicola* bicola);
+
+#endif /*bicola.h*/
